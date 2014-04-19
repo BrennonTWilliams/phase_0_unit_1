2e5a222212830c6161b74a9c6571b0fa0df27f26
Brennon <brennonw@gmail.com>
2014-04-19 15:40:01 -0500

    Update my_solution.js

--- week_3/2_js_variables_objects/my_solution.js
+++ week_3/2_js_variables_objects/my_solution.js
@@ -1,37 +1,39 @@
-// I paired [by myself, with:] on this challenge.
+// I paired by myself on this challenge.
 
 
 
 
 // Pseudocode
-// 
-// 
-// 
-// 
+//Judging by the driver code I know I need to define secretNumber, password, allowedIn, members
+// SET number secretNumber to 7
+// SET string password to "just open the door"
+// SET boolean allowedIn to false
+// SET array members to [John, placeholderName1, placeholderName2, Mary]
 
 
 // __________________________________________
 // Write your code below.
 
-
-
-
-
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = false;
+var members = ["John", "Brennon", "Sam", "Mary"];
 
 
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
 
-
+//I'm not really sure how to refactor this code, it seems like its as clean and simple as it could be.
+//All I did was create the variables and assign values according to the driver code given below.
 
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
-// 
-// 
+// I really enjoyed this assignment. I liked that I opened the file having no idea of what the objective was. I had 
+// to play detective and figure out what the purpose of the program was. My mistake was going into this assignment with
+// the mindset of "what is this program accomplishing?" Really, it was just a list of paramaters I needed to meet by
+// creating a few variables. The solution was a lot simpler than I thought it would be.
 // 
 // 
 
